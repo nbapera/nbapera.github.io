02d30bb3a09628c7a22ca97b214ee5f4043a3dd2
earfquake676 <earfquake676@proton.me>
2025-12-18 17:29:28 +0100

    first commit

---
+++ data/data.ts
@@ -0,0 +1,13 @@
+export const links = {
+  home: "/",
+  about: "/about",
+  work: "https://gifted3.com/",
+  contact: "https://gifted3.com/contact",
+  email: "mailto:petar.vuceticc3@gmail.com",
+  resume: "/resume",
+  sourceCode: "https://github.com/nbapera/nbapera.github.io",
+  github: "https://github.com/nbapera",
+  linkedin: "https://www.linkedin.com/in/petar-vucetic-b69a392b1",
+  twitter: "https://www.linkedin.com/in/petar-vucetic-b69a392b1/",
+  telegram: "https://t.me/tarpe1122",
+};
